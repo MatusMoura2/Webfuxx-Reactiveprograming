c6144e6af3da3689403957c3f166499352a53e50
Matus Moura <matus.moura333x2@gmail.com>
2024-09-07 09:38:38 -0300

    frist commit

---
+++ src/test/java/com/mouraforte/templatewebflux/TemplateWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouraforte.templatewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
